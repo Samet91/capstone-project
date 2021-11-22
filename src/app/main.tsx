9ef4e62b65e4faf626cf57376535be2e70d4dd34
Samet Uslu <samet_u@hotmail.de>
2021-11-22 12:32:02 +0100

    Setup done

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './globals.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import GlobalStyles from './GlobalStyles'
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.querySelector('#app')
-);
+)

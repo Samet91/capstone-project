a2436b14b89f8b2edce80714d553c94c7180bb55
Samet Uslu <samet_u@hotmail.de>
2021-11-24 13:16:18 +0100

    create Transaction story

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Expense from './components/Expense/Expense'
+import TransactionForm from './components/TransactionForm/TransactionForm'
+// import TransactionForm from './components/Transaction/TransactionForm'
 
 function App(): JSX.Element {
-  return <>
-    <Expense income={0} expense={0} />
-  </>
+  return (
+    <>
+      <Expense income={0} expense={0} />
+      <TransactionForm />
+    </>
+  )
 }
 
 export default App
---
+++ src/app/components/TransactionForm/Transaction.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import TransactionForm from './TransactionForm'
+
+export default {
+  title: 'Component/TransactionForm',
+  component: TransactionForm,
+}
+
+
+export const Regular = (): JSX.Element => (
+  <TransactionForm 
+    onNewTransaction={data =>  {
+
+      data.category = "Schuhe"
+      data.amount = 50
+      data.type = 
+    }
+  }
+    
+
+  />
+)

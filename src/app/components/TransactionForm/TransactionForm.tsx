af0fb90da4eba9100379c85076c77f55d8e17806
Samet Uslu <samet_u@hotmail.de>
2021-11-25 18:00:45 +0100

    test

--- src/app/components/TransactionForm/TransactionForm.tsx
+++ src/app/components/TransactionForm/TransactionForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { idGenerator } from '../Utils'
-
+import styled from 'styled-components'
 
 export type Transaction = {
   id: number
@@ -11,7 +11,6 @@ export type Transaction = {
 
 type TransactionFormProps = {
   onNewTransaction: (data: Transaction) => void
-
 }
 
 export default function TransactionForm({
@@ -32,7 +31,6 @@ export default function TransactionForm({
       amount: amount,
       type: type,
     }
-
     onNewTransaction(data)
 
     setCategory('')
@@ -41,58 +39,84 @@ export default function TransactionForm({
 
   return (
     <div>
-      <h2>Transaktion:</h2>
+      <H2>Transaktion:</H2>
       <form>
-        <input
-          type="text"
-          placeholder="Kategorie.."
-
-
-          maxLength={15}
-
-          value={category}
-          onChange={(event) => setCategory(event.target.value)}
-        />
-        <input
-          type="number"
-          placeholder="Betrag.."
-
-          value={amount}
-          onChange={(event) =>
-            setAmount(Math.abs(parseFloat(event.target.value)))
-          }
-        />
-        <button onClick={(event) => addTransaction('income', event)}>
-          Einnahmen
-        </button>
-        <button onClick={(event) => addTransaction('expense', event)}>
-          Ausgaben
-        </button>
+        <CatContainer>
+          <Input
+            type="text"
+            placeholder="Kategorie.."
+            required
+            maxLength={15}
+            value={category}
+            onChange={(event) => setCategory(event.target.value)}
+          />
+        </CatContainer>
+        <AmountContainer>
+          <Input
+            type="number"
+            placeholder="Betrag.."
+            required
+            maxLength={8}
+            value={amount}
+            onChange={(event) =>
+              setAmount(Math.abs(parseFloat(event.target.value)))
+            }
+          />
+        </AmountContainer>
+        <ButtonContainer>
+          <Button onClick={(event) => addTransaction('income', event)}>
+            Einnahmen
+          </Button>
+          <Button onClick={(event) => addTransaction('expense', event)}>
+            Ausgaben
+          </Button>
+        </ButtonContainer>
       </form>
     </div>
   )
 }
 
-// const   = styled.`
+const H2 = styled.h2`
+  font-size: 1.25rem;
+  font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+  line-height: 1.6;
+  letter-spacing: 0.1em;
+  margin: 0;
+  color: #131111;
+`
 
-// `
+const CatContainer = styled.div`
+  display: inline-flex;
+  position: relative;
+  min-width: 0;
+  flex-direction: column;
+  vertical-align: top;
+  margin-top: 10px;
+  width: 100%;
+`
 
-// const   = styled.`
+const AmountContainer = styled.div`
+  display: inline-flex;
+  position: relative;
+  min-width: 0;
+  flex-direction: column;
+  vertical-align: top;
+  margin-top: 10px;
+  width: 100%;
+`
 
-// `
+const Input = styled.input`
+  padding: 10px;
+  outline: none;
+  border-color: aliceblue;
+  border-radius: 5px;
+`
 
-// const   = styled.`
+const ButtonContainer = styled.div`
+  margin: 16px 0px 0px;
+`
 
-// `
-
-// const   = styled.`
-
-// `
-
-// const   = styled.`
-
-// `
-
-// const   = styled.`
-
-// `
+const Button = styled.button`
+  width: 50%;
+  padding: 5px;
+`

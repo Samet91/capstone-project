010e91f957d644474fe0103728070a38ea6358d1
Samet Uslu <samet_u@hotmail.de>
2021-11-30 18:39:57 +0100

    create Button

---
+++ src/app/components/Button/Button.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+  padding: 0.5rem 1rem;
+  border-radius: 8px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`
+export default Button

cace2e8aab9f9db28ebfe81f406385aa02a4a340
Samet Uslu <samet_u@hotmail.de>
2021-11-30 18:40:51 +0100

    import ArrowIcon

--- src/app/Pages/Dashboard.tsx
+++ src/app/Pages/Dashboard.tsx
@@ -8,6 +8,7 @@ import type { TransactionProps } from '../../types'
 import Button from '../components/Button/Button'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import ArrowIconRight from '../components/Icons/ArrowIconRight'
 
 export default function Dashboard(): JSX.Element {
   const [transactions, setTransactions] = useLocalStorage<TransactionProps[]>(
@@ -44,13 +45,33 @@ export default function Dashboard(): JSX.Element {
       />
       <TransactionForm onNewTransaction={handleNewTransaction} />
       <Link to="AddCosts">
-        <StyledButton>Costs</StyledButton>
+        <StyledButton>
+          <ButtonText>Fixkosten</ButtonText>
+          <Icon>
+            <ArrowIconRight />
+          </Icon>
+        </StyledButton>
       </Link>
     </>
   )
 }
 
 const StyledButton = styled(Button)`
-    display: flex;
-    
+  font-weight: bold;
+  font-size: 1rem;
+  background-color: steelblue;
+  border: none;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+`
+
+const ButtonText = styled.span`
+  align-self: center;
+  line-height: 1.6;
+  letter-spacing: 0.1em;
+`
+
+const Icon = styled.span`
+  grid-column: 6/7;
+  margin-right: -7px;
 `
---
+++ src/app/components/Icons/ArrowIconRight.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function ActionLinkIcon(
+  props: React.SVGProps<SVGSVGElement>
+): JSX.Element {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path d="M10.024 4h6.015l7.961 8-7.961 8h-6.015l7.961-8-7.961-8zm-10.024 16h6.015l7.961-8-7.961-8h-6.015l7.961 8-7.961 8z" />
+    </svg>
+  )
+}

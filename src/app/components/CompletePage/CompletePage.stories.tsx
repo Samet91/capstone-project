0b8d6e45a81b86cd00fd20ec121d4ad4e53a5fcd
Samet Uslu <samet_u@hotmail.de>
2021-11-30 16:14:33 +0100

    completepage move content to dashboard

--- src/app/components/CompletePage/CompletePage.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import CompletePage from './CompletePage'
-
-export default {
-  title: 'Component/Completepage',
-  component: CompletePage,
-}
-
-export const Regular = (): JSX.Element => <CompletePage />

2c138ff2941626c4596a3362edc5112e5bf83769
Samet Uslu <samet_u@hotmail.de>
2021-12-01 10:37:10 +0100

    page 2 create form

--- src/app/Pages/AddCosts.tsx
+++ src/app/Pages/AddCosts.tsx
@@ -1,5 +1,23 @@
 import React from 'react'
+import type { Costs } from '../../types'
+import CostsForm from '../components/CostsForm/CostsForm'
+import useLocalStorage from '../hooks/useLocalStorage'
 
 export default function AddCosts(): JSX.Element {
-  return <div></div>
+  const [cost, setCost] = useLocalStorage<Costs[] | null>('cost', null)
+
+  function handleSubmit(costs: Costs): void {
+    const newCosts = costs
+    if (cost) {
+      setCost([...cost, newCosts])
+    } else {
+      setCost([newCosts])
+    }
+  }
+
+  return (
+    <main>
+      <CostsForm onSubmit={handleSubmit} />
+    </main>
+  )
 }

74bcf50edb76bf69633d240474d63055e33289af
Samet Uslu <samet_u@hotmail.de>
2021-12-02 14:48:39 +0100

    implement post in server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,22 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import express from 'express'
-import { connectDatabase } from './app/hooks/database'
+import { connectDatabase, getCollection } from './app/hooks/database'
 
 if (!process.env.MONGODB_URI) {
   throw new Error('No MongoDB URL dotenv variable')
 }
 
 const app = express()
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3000
 
 app.use(express.json())
+
+app.post('/costs', async (req, res) => {
+  const newCosts = req.body
+  await getCollection().insertOne(newCosts)
+  res.status(200).send('Newcost was added')
+})
 
 app.get('/api/hello', (_request, response) => {
   response.json({ message: 'Hello API!' })

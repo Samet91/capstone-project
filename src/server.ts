8ac6938040c5c24cc94ebd5ae526e0ea08cdc8ce
Samet Uslu <samet_u@hotmail.de>
2021-12-07 14:11:19 +0100

    add get function

--- src/server.ts
+++ src/server.ts
@@ -9,20 +9,25 @@ if (!process.env.MONGODB_URI) {
 }
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.use(express.json())
 
-app.post('/costs', async (req, res) => {
+app.post('/api/costs', async (req, res) => {
   const newCosts = req.body
   await getCollection().insertOne(newCosts)
   res.status(200).send('Newcost was added')
 })
 
-app.delete('/delete/:_id', async (req, res) => {
+app.delete('/api/delete/:_id', async (req, res) => {
   const costId = req.params._id
   await getCollection().deleteOne({ _id: Number(costId) })
   res.status(200).send('deleted one transaction')
+})
+
+app.get('/api/costs', async (_req, res) => {
+  const existingCosts = await getCollection().find().toArray()
+  res.send(existingCosts)
 })
 
 app.get('/api/hello', (_request, response) => {
